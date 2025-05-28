a64463e6fec509c4ac14c3d73b8c3c40f55caca9
Darlan Rodrigues Schmeller <darlanschimeller@gmail.com>
2025-05-28 13:33:16 -0300

    Study string concatenation in PHP

--- php-sandbox/01-variables-data-types/03-data-types/index.php
+++ php-sandbox/01-variables-data-types/03-data-types/index.php
@@ -12,3 +12,90 @@
 - NULL
 - Resource
 */
+
+
+// ----------
+// String
+
+$name = 'Brad Traversy';
+$name2 = "John Doe";
+
+var_dump($name);
+echo '<br />';
+echo($name);
+echo '<br>';
+echo getType($name2);
+echo '<br> ---------- <br> ';
+
+
+
+// ----------
+// Integer
+
+$age = 35;
+$age = -35;
+echo($age);
+echo '<br>';
+var_dump($age);
+echo '<br> ---------- <br> ';
+
+
+
+// ----------
+// Float
+
+$rating = 4.5;
+echo($rating);
+echo '<br>';
+var_dump($rating);
+echo '<br> ---------- <br> ';
+
+
+
+// ----------
+// Boolean
+
+$boolean = true;
+echo($boolean);
+echo '<br>';
+var_dump($boolean);
+echo '<br> ---------- <br> ';
+
+
+
+// ----------
+// Array
+
+$array = [1,2,3,4];
+echo($array);
+echo '<br>';
+var_dump($array);
+echo '<br> ---------- <br> ';
+
+
+
+// ----------
+// Object
+
+$object = new stdClass();
+var_dump($object);
+echo '<br> ---------- <br> ';
+
+
+
+// ----------
+// Null
+
+$null = null;
+echo($null);
+var_dump($null);
+echo '<br> ---------- <br> ';
+
+
+
+// ----------
+// Resource
+
+// $file = fopen('sample.txt','r');
+// var_dump($file);
+// echo '<br> ---------- <br> ';
---
+++ php-sandbox/01-variables-data-types/04-strings-concatenation/index.php
@@ -0,0 +1,35 @@
+<?php
+  $firstName = 'John';
+  $lastName = 'Doe';
+
+  $fullName = $firstName . ' ' . $lastName; // To concatenate strings in Php we use .
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://cdn.tailwindcss.com"></script>
+  <title>Learn PHP From Scratch</title>
+</head>
+
+<body class="bg-gray-100">
+  <header class="bg-blue-500 text-white p-4">
+    <div class="container mx-auto">
+      <h1 class="text-3xl font-semibold">Learn PHP From Scratch</h1>
+    </div>
+  </header>
+  <div class="container mx-auto p-4 mt-4">
+    <div class="bg-white rounded-lg shadow-md p-6">
+      <!-- Output -->
+      <?= $fullName . '<br>' ?>
+      <?= 'Hello, my full name is ' . $fullName ?>
+      <?= "Hello, my name is $fullName" // Variable Interpolation ?>
+      <?= 'Hello my name is \'john\'' // Escaping the ' character ?>
+    </div>
+  </div>
+</body>
+
+</html>
