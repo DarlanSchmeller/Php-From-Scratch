6ee404b13aaf3753d0f68666b5fd23aaade0b726
Darlan Rodrigues Schmeller <darlanschimeller@gmail.com>
2025-05-30 15:34:56 -0300

    Work with array creation and element manipulation

---
+++ php-sandbox/02-arrays-and-iteration/01-intro-to-arrays/index.php
@@ -0,0 +1,37 @@
+<?php
+
+// Function to print values as they are written with <pre>
+function inspect($value) {
+    echo '<pre>';
+    var_dump($value);
+    echo '</pre>';
+}
+
+// Creating an array, both methods accomplish the same
+$names = array('John', 'Jack', 'Jill');
+$numbers = [1,2,3,4,5,6];
+
+// Just printing our arrays
+inspect($names);
+inspect($numbers);
+
+// Accessing value in array by index
+echo $names[2];
+
+// Add element to array to specific index
+$numbers[6] = 144;
+$numbers[3] = 300;
+
+// Add element to the end of array
+$numbers[] = 155;
+inspect($numbers);
+
+// Add element to the start of array with this function
+array_unshift($numbers, 188);
+inspect($numbers);
+
+// Remove element from array using index
+unset($numbers[3]);
+
+// Reindex array since the index will be empty otherwise, array_values returns the array and is not in place
+$numbers = array_values($numbers);
