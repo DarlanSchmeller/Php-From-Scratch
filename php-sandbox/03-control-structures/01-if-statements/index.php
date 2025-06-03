9d48cf97de6b9752daa3dd439996ccdee890580a
Darlan Rodrigues Schmeller <darlanschimeller@gmail.com>
2025-06-03 13:20:58 -0300

    Explore if/else and logical branching in PHP

---
+++ php-sandbox/03-control-structures/01-if-statements/index.php
@@ -0,0 +1,36 @@
+<?php
+
+$age = 19;
+
+// If statement
+if ($age >= 21) {
+    echo 'You are old enough to drink in the US';
+}
+echo '<hr>';
+
+// If-Else
+if ($age >= 21) {
+    echo 'You are old enough to drink in the US';
+} else {
+    echo 'You are NOT old enough to drink in the US';
+}
+echo '<hr>';
+
+// Nested if statement
+if ($age >= 21) {
+    echo 'You are old enough to vote and drink in the US';
+} else {
+    if ($age > 18) {
+        echo 'You are NOT old enough to vote in the US';
+    } else {
+        echo 'You are NOT old enough to vote or drink in the US';
+    }
+}
+echo '<hr>';
+
+// If-Else-If
+if ($age >= 21) {
+    echo 'You are old enough to vote and drink in the US';
+} else if ($age >= 18) {
+    echo 'You are NOT old enough to vote in the US';
+} 
