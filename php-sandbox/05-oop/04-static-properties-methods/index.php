52c668c86a981b35332c65135710b12e746c6f54
Darlan Rodrigues Schmeller <darlanschimeller@gmail.com>
2025-06-06 11:21:51 -0300

    Implement addValues method to MathUtility class and create example

--- php-sandbox/05-oop/04-static-properties-methods/index.php
+++ php-sandbox/05-oop/04-static-properties-methods/index.php
@@ -5,8 +5,13 @@ class MathUtility
     // Being static means it's shared by all instances of the class,
     // and can be accessed without creating an object, like ClassName::$pi.
     public static $pi = 3.14;
+
+    public static function add(...$nums) {
+        return array_sum($nums);
+    }
 }
 
 $mathUtil1 = new MathUtility();
 
-echo MathUtility::$pi;  // We can access static methods without instancing the class
+echo MathUtility::$pi . '<br>';  // We can access static methods without instancing the class
+echo MathUtility::add(1,2,3,4,5);
