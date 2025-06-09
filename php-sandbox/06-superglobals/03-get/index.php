f431064dc704fdb64007fa6a04b95ea25695631f
Darlan Rodrigues Schmeller <darlanschimeller@gmail.com>
2025-06-09 17:42:18 -0300

    Add filtered lists using query values and array_filter

---
+++ php-sandbox/06-superglobals/03-get/index.php
@@ -0,0 +1,7 @@
+<?php
+// Add a query parameter to the URL in the browser's address bar:
+// http://localhost:8000/?name=John
+
+var_dump($_GET);
+echo isset($_GET['name']) ? $_GET['name'] : null;
+echo htmlspecialchars($_GET['name'] ?? '');
