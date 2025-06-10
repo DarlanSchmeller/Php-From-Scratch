4485480b5f2b257f6f1235c0f67c80745815d343
Darlan Rodrigues Schmeller <darlanschimeller@gmail.com>
2025-06-10 10:43:35 -0300

    Use REQUEST superglobal to get queries and POST data

--- php-sandbox/06-superglobals/05-request/index.php
+++ php-sandbox/06-superglobals/05-request/index.php
@@ -1,3 +1,15 @@
+<?php
+    echo $_REQUEST['name'] ?? '';
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_REQUEST['submit'])) 
+    {
+        $name = htmlspecialchars($_REQUEST['name']) ?? '';
+        $age = htmlspecialchars($_REQUEST['age']) ?? '';
+        
+        $submitted = true;
+    }
+?>
+
 <form method="post">
   <div>
     <label for="name">Name:</label>
@@ -9,3 +21,18 @@
   </div>
   <input type="submit" name="submit" value="Submit">
 </form>
+
+<?php if ($submitted) : ?>
+    <!-- Display submitted data -->
+    <div class="mt-6 p-4 border rounded bg-gray-200">
+        <h2 class="text-lg font-semibold">Job Listing:</h2>
+        <p>
+            <strong>Title:</strong>
+            <?= $name ?> 
+        </p>
+        <p>
+            <strong>Description:</strong>
+            <?= $age ?> 
+        </p>
+    </div>
+<?php endif; ?>
